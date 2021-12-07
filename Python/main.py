044507e0a252b8d395e00741e0c78af42d39adf9
Juan Di Pasquo <juandipasquo@gmail.com>
2021-12-07 14:25:09 -0300

    Declarated constructor method on Python

--- Python/main.py
+++ Python/main.py
@@ -1,6 +1,16 @@
+from car import Car
+
 def run():
     print('Hola mundo')
+    car = Car()
+    car.license = "AMS321"
+    car.driver = "Andres Herrera"
+    print(vars(car))
 
+    car2 = Car()
+    car2.license = "GFS851"
+    car2.driver = "Martha Herrera"
+    print(vars(car2))
 
 if __name__ == '__main__':
     run()
