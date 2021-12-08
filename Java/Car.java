e890a2264f3deba891f1132a4cdbd4d71fb2f856
Juan Di Pasquo <juandipasquo@gmail.com>
2021-12-08 13:36:45 -0300

    Declared a contructor method with this. on Java

--- Java/Car.java
+++ Java/Car.java
@@ -3,10 +3,15 @@ package Java;
 class Car {
     Integer id;
     String license;
-    String driver;
+    Account driver;
     Integer passengers;
 
+    public Car(String license, Account driver){
+        this.license = license;
+        this.driver = driver;
+    }
+
     void printDataCar() {
-        System.out.println("Lincense: " + license + " Driver: " + driver);
+        System.out.println("license: " + license + " Driver: " + driver.name);
     }
 }
--- Java/Main.java
+++ Java/Main.java
@@ -3,15 +3,11 @@ package Java;
 class Main {
     public static void main(String[] args) {
         System.out.println("Car class");
-        Car car = new Car();
-        car.license = "AMQ132";
-        car.driver = "Andres Herrera";
+        Car car = new Car("AMQ132", new Account("Andres Herrera", "AND123"));
         car.passengers = 4;
         car.printDataCar();
 
-        Car car2 = new Car();
-        car2.license = "QWF462";
-        car2.driver = "Paula Herrera";
+        Car car2 = new Car("QWF462", new Account("Paula Herrera", "PHN456"));
         car2.passengers = 3;
         car2.printDataCar();
     }
