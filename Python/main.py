5f63d8584052bb1c30efafd9d7ac8a1c27dcac9a
Juan Di Pasquo <juandipasquo@gmail.com>
2021-12-11 13:37:44 -0300

    Added a function to print car data

--- Python/main.py
+++ Python/main.py
@@ -2,21 +2,21 @@ from car import Car
 from account import Account
 from uberX import UberX
 
+def print_car_data(car_name):
+    print('\n', vars(car_name))
+    print(vars(car_name.driver))
 
 def run():
     print('Car class')
 #   Car1
     car = Car("AMS321", Account("Andres Herrera", "45613A"))
-    print('\n', vars(car))
-    print(vars(car.driver))
+    print_car_data(car)
 #   Car2
     car2 = Car("GFS851", Account("Paula Herrera", "45612B"))
-    print('\n', vars(car2))
-    print(vars(car2.driver))
+    print_car_data(car2)
 #   UberX
     car3 = UberX("AMS321", Account("Andres Herrera", "45613A"), 'Hyundai', 'H1')
-    print('\n', vars(car3))
-    print(vars(car3.driver))
+    print_car_data(car3)
 
 if __name__ == '__main__':
     run()
